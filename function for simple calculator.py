3347da565cf9ef37db8aa009b510621aee420e02
RoshanBP <bproshan93@gmail.com>
2025-03-12 15:20:27 +0530

    Add files via upload

---
+++ function for simple calculator.py
@@ -0,0 +1,13 @@
+def calculator(num1, num2, operation):
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 == 0:
+            raise ValueError("Cannot divide by zero")
+        return num1 / num2
+    else:
+        raise ValueError("Invalid operation")
