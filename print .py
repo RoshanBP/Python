0c4c7216b1da31a6fe2be2f67fd649ade9270078
RoshanBP <bproshan93@gmail.com>
2025-03-12 12:25:31 +0530

    Add files via upload

---
+++ print .py
@@ -0,0 +1,12 @@
+#printing variable
+a=1
+b=2
+print(a,b)
+
+#printing string
+print("hello world")
+
+#printing string with variable (formatted string)
+a=1
+b=2
+print("the value of a is",a,"and the value of b is",b)
