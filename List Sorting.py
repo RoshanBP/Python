98787ca803cf85b588617bb6c824447f52367e4f
RoshanBP <bproshan93@gmail.com>
2025-03-13 14:18:49 +0530

    Add files via upload

---
+++ List Sorting.py
@@ -0,0 +1,9 @@
+numbers = [5, 2, 8, 1, 9]
+
+# Ascending order
+numbers.sort()
+print("Ascending order:", numbers)
+
+# Descending order
+numbers.sort(reverse=True)
+print("Descending order:", numbers)
