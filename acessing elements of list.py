77b27f90e4f20a33f4a1a9fa6ea80d4f4547267f
RoshanBP <bproshan93@gmail.com>
2025-03-13 14:12:58 +0530

    Add files via upload

---
+++ acessing elements of list.py
@@ -0,0 +1,14 @@
+my_list = [1, 2, 3, 4, 5]
+
+# Accessing the first element
+first_element = my_list[0]
+print("First element:", first_element)
+
+# Accessing the last element
+last_element = my_list[-1]
+print("Last element:", last_element)
+
+# Accessing the middle element
+middle_index = len(my_list) // 2
+middle_element = my_list[middle_index]
+print("Middle element:", middle_element)
