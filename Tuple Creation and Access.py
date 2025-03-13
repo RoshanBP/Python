40b6d45801f39d46992dfc29269281cb74ed04f2
RoshanBP <bproshan93@gmail.com>
2025-03-13 14:29:23 +0530

    Add files via upload

---
+++ Tuple Creation and Access.py
@@ -0,0 +1,6 @@
+my_tuple = (1, "hello", 3.14, True, [1, 2, 3])
+print(my_tuple[0])  # Accessing the first element
+print(my_tuple[1])  # Accessing the second element
+print(my_tuple[2])  # Accessing the third element
+print(my_tuple[3])  # Accessing the fourth element
+print(my_tuple[4])  # Accessing the fifth element
